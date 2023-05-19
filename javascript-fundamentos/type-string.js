c456249d27313e5c2eda0b176930bb037d9ebf4a
Luciano <lucianossweb@gmail.com>
2023-05-19 14:44:44 -0400

    Exemplos basicos

---
+++ javascript-fundamentos/type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá Mundo";
+const texto2 = "oi";
+const senha = "senhaSuperSeguras";
+const StingDeNumeros = "32564";
+
+const citacao = "O teste acontece";
+
+console.log(citacao);
+
+// concatenação (+)
+
+console.log(texto1+texto2);
+
+// Template string Ou template literal
+
